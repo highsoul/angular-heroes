7e6e738132f89f47eabdf2ee5a8f025db2dfdb9b
highsoul <highsoul3301@163.com>
2016-11-07 16:13:23 +0800

    add http -> save hero function

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,20 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
-import { Hero } from './models/hero'
-import { HEROES } from './mock-heroes'
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './models/hero';
 
 @Injectable()
 export class HeroService {
+
+	private heroesUrl = 'app/heroes'; //URL to web api
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	constructor(private http: Http){}
 
     getHero(id: number): Promise<Hero>{
         return this.getHeroes().then(heroes => heroes.find(hero => hero.id == id));
     }
 
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+        		.toPromise()
+        		.then(response => response.json().data as Hero[])
+        		.catch(this.handleError);
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise<Hero[]> (resolve => setTimeout(resolve, 5000)).then(() => this.getHeroes());
     }
+
+
+    private handleError(error: any): Promise<any> {
+	    console.error('An error occurred', error); // for demo purposes only
+	    return Promise.reject(error.message || error);
+	}
+
+	update(hero: Hero): Promise<Hero> {
+	  const url = `${this.heroesUrl}/${hero.id}`;
+	  return this.http
+	    .put(url, JSON.stringify(hero), {headers: this.headers})
+	    .toPromise()
+	    .then(() => hero)
+	    .catch(this.handleError);
+}
 }
